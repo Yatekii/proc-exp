c8fcfb794b288ed0a21f9cdb009c023a15ee6aa2
Noah Huesser <admin@yatekii.ch>
2019-04-07 00:06:52 +0200

    Added demo syntax for dynamic lists + Needs suport for lifetimes

--- user/src/main.rs
+++ user/src/main.rs
@@ -12,16 +12,11 @@ frame! {
         field4 [3][1..5]: u8,
         // byte 4 bits 0 through 1 as enum KekHint; KekHint needs to impl Serializable
         field5 [4][0..1]: KekHint,
-        // // dependant enum with it's body ranging from byte 5 to 42
-        // // type is Kek and the hinter is field4
-        // // parsed variant depends on the variant field4 holds
-        // // Keks variant needs to hold a struct with the same name as the variant
-        // field6 [5..42]: Kek: field5,
-        // // A bool, characterized by byte 7, bit 0
-        // field7 [7][0]: bool,
         // this field will automatically be Option<char> where char is byte 4
         // and the option state is represented by field6
         field8 [4]: char -> [7][0]: bool,
+        // A dynamic list where the lenght is stored as an u8 in byte 7
+        field9 [4]: &[u8] -> [7]: u8,
     }
 }
 
